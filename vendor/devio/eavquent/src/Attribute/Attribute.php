78eb15272d53ad136f35264b02f0755c0f39ce36
Alexander Litvinenko <litvinenko1706@gmail.com>
2016-04-11 00:36:01 +0300

    TEMPorarily add vendor/devio/eavquent/src/Attribute/Attribute.php

---
+++ vendor/devio/eavquent/src/Attribute/Attribute.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace Devio\Eavquent\Attribute;
+
+use Devio\Eavquent\Events\AttributeWasSaved;
+use Illuminate\Database\Eloquent\Model;
+
+class Attribute extends Model
+{
+    /**
+     * Model timestamps.
+     *
+     * @var bool
+     */
+    public $timestamps = false;
+
+    /**
+     * Fillable attributes.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'code', 'label', 'model', 'entity', 'default_value', 'collection',
+    ];
+
+    /**
+     * Attribute constructor.
+     *
+     * @param array $attributes
+     */
+    public function __construct(array $attributes = [])
+    {
+        $this->setTable(eav_table('attributes'));
+
+        parent::__construct($attributes);
+    }
+
+    /**
+     * Registering events.
+     */
+    public static function boot()
+    {
+        parent::boot();
+
+        static::saved(AttributeWasSaved::class . '@handle');
+    }
+
+    /**
+     * Get the attribute code name.
+     *
+     * @return mixed
+     */
+    public function getCode()
+    {
+        return $this->getAttribute('code');
+    }
+
+    /**
+     * Get the model class name.
+     *
+     * @return mixed
+     */
+    public function getModel()
+    {
+        return $this->getAttribute('model');
+    }
+
+    /**
+     * Return the model class.
+     *
+     * @return mixed
+     */
+    public function getModelInstance()
+    {
+        $class = $this->getAttribute('model');
+
+        return new $class;
+    }
+
+    /**
+     * Check if attribute is multivalued.
+     *
+     * @return bool
+     */
+    public function isCollection()
+    {
+        return (bool) $this->getAttribute('collection');
+    }
+
+}
